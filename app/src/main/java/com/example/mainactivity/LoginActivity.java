be0885dc985cb1012ad88b3451939fb3da72d126
GabrielVilasboa <gabrieldecarvalhovilasboa@gmail.com>
2023-01-20 22:25:20 -0300

    add classes in project

--- app/src/main/java/com/example/mainactivity/LoginActivity.java
+++ app/src/main/java/com/example/mainactivity/LoginActivity.java
@@ -6,14 +6,10 @@ import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Login login;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        login = findViewById(R.id.login);
-        login.setBackgroundColor(R.color.colorBlack);
     }
 }
