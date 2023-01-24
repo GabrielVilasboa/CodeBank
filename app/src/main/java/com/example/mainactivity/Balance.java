7d918902ee3c56393684a112a929e25e37403d93
Rodrigo <rleandro800@gmail.com>
2023-01-23 22:28:14 -0300

    add balance and client class

---
+++ app/src/main/java/com/example/mainactivity/Balance.java
@@ -0,0 +1,14 @@
+package com.example.mainactivity;
+
+import java.util.ArrayList;
+
+public class Balance {
+    Float name;
+    //ArrayList<Historic> historic = new ArrayList();
+    public Balance(){
+
+    }
+    void movimentationBalance() {
+
+    }
+}
