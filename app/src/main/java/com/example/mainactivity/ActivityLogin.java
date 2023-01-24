c90e5e0943ceb7f1b5381c1e7590822ec3ddfdf7
Felipe Magro <felipe.am2003@hotmail.com>
2023-01-23 22:31:02 -0300

    add ActivityLogin

---
+++ app/src/main/java/com/example/mainactivity/ActivityLogin.java
@@ -0,0 +1,30 @@
+package com.example.mainactivity;
+
+public class ActivityLogin {
+    String cpf, password;
+
+    public ActivityLogin(){
+        switch(){
+            case R.id.:
+
+                ;
+        }
+    }
+
+    private void VerifyCpf(){
+
+    }
+
+    private void VerifyPassword(){
+
+    }
+
+    private void Register(){
+
+    }
+
+    private void ForgetPassword(){
+
+    }
+
+}
